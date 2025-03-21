fb19e9783b72e0bd14b588018090e182e69f9f9b
Anusha2456 <anushakurakula24@gmail.com>
2025-03-21 11:26:37 +0530

    commt

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Register() {
+  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState({});
+  const handleSubmit = () => {
+    setProducts([...products, product]);
+  };
   return (
-    <div>
+    <div className="App-Row">
+      <div>
       <h2>Registration Form</h2>
-      <p><input type="text" placeholder='Enter name'></input></p>
-      <p><input type="text" placeholder='Email address'></input></p>
-      <p><input type="password" placeholder='New password'></input></p>
-      <p><button>Submit</button></p>
+      <p><input type="text" placeholder='Enter name' 
+      onChange={(e) => setProduct({ ...product, name: e.target.value })}></input></p>
+      <p><input type="text" placeholder='Email address' 
+      onChange={(e) => setProduct({ ...product, price: e.target.value })}></input></p>
+      <p><input type="password" placeholder='New password'
+       onChange={(e) => setProduct({ ...product, quantity: e.target.value })}></input></p>
+      
+      <p><button onClick={handleSubmit}>Submit</button>
+      </p>
       <p>
         <Link to="../login">Already a member? Log In</Link>
       </p>
+      </div>
+      <div>
+        <h4>Details</h4>
+        <table border="1">
+          {products &&
+            products.map((value, index) => (
+              <tr>
+                <td>{value.name}</td>
+                <td>{value.price}</td>
+                <td>{value.quantity}</td>
+              </tr>
+            ))}
+        </table>
+      </div> 
     </div>
   )
 }
