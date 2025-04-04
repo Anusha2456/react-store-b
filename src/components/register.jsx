84e0e581549852c33cb4c0301c65194ba4ba6d64
Anusha2456 <anushakurakula24@gmail.com>
2025-04-04 10:05:44 +0530

    update

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,41 +1,68 @@
+import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import "./Register.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { appContext } from "../App";
 export default function Register() {
-  const [products, setProducts] = useState([]);
-  const [product, setProduct] = useState({});
+  const { user, setUser, users, setUsers,cart } = useContext(appContext);
+  const [msg, setMsg] = useState();
+  const Navigate = useNavigate();
   const handleSubmit = () => {
-    setProducts([...products, product]);
+    const found = users.find((value) => value.email === user.email);
+    if (found) {
+      setMsg("User already exists");
+    } else {
+      setUsers([...users, user]);
+      setMsg();
+      Object.keys(cart).length > 0 ? Navigate("/cart") : Navigate("/");
+    }
+  };
+  const handleDelete = (email) => {
+    setUsers(users.filter((value) => value.email != email));
   };
   return (
-    <div className="App-Row">
+    <div className="App-Register-Row">
       <div>
-      <h2>Registration Form</h2>
-      <p><input type="text" placeholder='Enter name' 
-      onChange={(e) => setProduct({ ...product, name: e.target.value })}></input></p>
-      <p><input type="text" placeholder='Email address' 
-      onChange={(e) => setProduct({ ...product, price: e.target.value })}></input></p>
-      <p><input type="password" placeholder='New password'
-       onChange={(e) => setProduct({ ...product, quantity: e.target.value })}></input></p>
-      
-      <p><button onClick={handleSubmit}>Submit</button>
-      </p>
-      <p>
-        <Link to="../login">Already a member? Log In</Link>
-      </p>
+        <h2>Registration Form</h2>
+        {msg}
+        <p>
+          <input
+            type="text"
+            placeholder="Enter name"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          ></input>
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Email address"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          ></input>
+        </p>
+        <p>
+          <input
+            type="password"
+            placeholder="New password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          ></input>
+        </p>
+        <p>
+          <button onClick={handleSubmit}>Submit</button>
+        </p>
+        <p>
+          <Link to="../login">Already a member? Log In</Link>
+        </p>
       </div>
       <div>
-        <h4>Details</h4>
-        <table border="1">
-          {products &&
-            products.map((value, index) => (
-              <tr>
-                <td>{value.name}</td>
-                <td>{value.price}</td>
-                <td>{value.quantity}</td>
-              </tr>
-            ))}
-        </table>
-      </div> 
+        {users &&
+          users.map((value, index) => (
+            <li>
+              {value.name}-{value.email}-{value.password}
+              <button onClick={() => handleDelete(value.email)}>Delete</button>
+            </li>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
